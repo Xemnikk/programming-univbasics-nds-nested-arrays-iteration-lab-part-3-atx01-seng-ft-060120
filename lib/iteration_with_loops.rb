7e0c4a59c016c74277a5c46e64247cacf67ccb08
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-07 17:26:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,8 +9,9 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do 
       if src[row_index][element_index] = String
       sum += src[row_index][element_index].to_s
-      element_index +=1 
+       
     end 
+    
     row_index += 1 
 end
 return sum
