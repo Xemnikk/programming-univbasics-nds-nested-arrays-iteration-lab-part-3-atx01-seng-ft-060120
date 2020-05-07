dfbed59b4e6f38787120050a2f54728ff773917f
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-07 17:25:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
   while row_index < src.count do 
     element_index = 0 
     while element_index < src[row_index].count do 
-      if src[row_index][element_index].respond_to? (:to_s)
+      if src[row_index][element_index] = String
       sum += src[row_index][element_index].to_s
       element_index +=1 
     end 
